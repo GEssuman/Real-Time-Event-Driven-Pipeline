2fcea1f2cbe6c58e58e7a68970e962f7f66f268d
GEssuman <godsaves.essuman@amalitechtraining.org>
2025-07-03 11:05:04 +0000

    implement load to dynamodb

--- services/transformation-task/transform.py
+++ services/transformation-task/transform.py
@@ -2,6 +2,8 @@ from pyspark.sql import SparkSession
 import os
 from pyspark.sql import functions as F
 import logging
+import boto3
+from decimal import Decimal
 
 
 def category_level_kpi(order_items_df, products_df):
@@ -13,13 +15,18 @@ def category_level_kpi(order_items_df, products_df):
     )
     joined_df = joined_df.withColumn("order_date", F.to_date("created_at"))
     kpi_df = joined_df.groupBy("category", "order_date").agg(
-        F.sum("sale_price").alias("daily_revenue"),
-        F.countDistinct("order_id").alias("num_orders"),
-        F.sum(F.when(F.col("status") == "returned", 1).otherwise(0)).alias("num_returns")
-    ).withColumn("avg_order_value", F.col("daily_revenue") / F.col("num_orders")) \
-    .withColumn("avg_return_rate", (F.col("num_returns") / F.col("num_orders")) * 100) \
-    .select("category", "order_date", "daily_revenue", "avg_order_value", "avg_return_rate")
-
+    F.round(F.sum("sale_price"), 2).alias("daily_revenue"),
+    F.countDistinct("order_id").alias("num_orders"),
+    F.sum(F.when(F.col("status") == "returned", 1).otherwise(0)).alias("num_returns")
+    ).withColumn(
+        "avg_order_value", F.round(F.col("daily_revenue") / F.col("num_orders"), 2)
+    ).withColumn(
+        "avg_return_rate", F.round(F.col("num_returns") / F.col("num_orders") * 100, 2)
+    ).select(
+        "category", "order_date", "daily_revenue", "avg_order_value", "avg_return_rate"
+    )
+    
+    write_to_dynamo(kpi_df.collect(), "category_level")
     return kpi_df
 
 def order_level_kpi(orders_df):
@@ -28,16 +35,62 @@ def order_level_kpi(orders_df):
 
     # KPI aggregations
     kpi_df = df.groupBy("order_date").agg(
-        F.countDistinct("order_id").alias("total_orders"),
-        F.sum("num_of_item").alias("total_items_sold"),
-        (F.sum(F.when(F.col("status") == "returned", 1).otherwise(0)) / F.countDistinct("order_id")).alias("return_rate"),
-        F.countDistinct("user_id").alias("unique_customers")
+    F.countDistinct("order_id").alias("total_orders"),
+    F.sum("num_of_item").alias("total_items_sold"),
+    F.round(
+        F.sum(F.when(F.col("status") == "returned", 1).otherwise(0)) / F.countDistinct("order_id") * 100,
+        2
+    ).alias("return_rate"),
+    F.countDistinct("user_id").alias("unique_customers")
     ).select(
         "order_date", "total_orders", "total_items_sold", "return_rate", "unique_customers"
     )
+
+    write_to_dynamo(kpi_df.collect(), "order_level")
     return kpi_df
 
 
+def write_to_dynamo(data, kpi_type):
+    """
+    Dynamically write KPIs to the appropriate DynamoDB table.
+    """
+    dynamodb = boto3.resource("dynamodb", region_name="eu-north-1")
+
+    if kpi_type == "order_level":
+        table = dynamodb.Table("ecommerce_order_kpis")
+    elif kpi_type == "category_level":
+        table = dynamodb.Table("ecommerce_category_kpis")
+    else:
+        raise ValueError("Unsupported KPI type")
+    
+
+    for row in data:
+        try:
+            item = {}
+
+            if kpi_type == "order_level":
+                item = {
+                    "order_date": row["order_date"].strftime("%Y-%m-%d"),
+                    "total_orders": int(row["total_orders"]),
+                    "total_revenue": Decimal(str(row["total_revenue"])),
+                    "total_items_sold": int(row["total_items_sold"]),
+                    "return_rate": Decimal(str(row["return_rate"])),
+                    "unique_customers": int(row["unique_customers"])
+                }
+
+            elif kpi_type == "category_level":
+                item = {
+                    "category": row["category"],
+                    "order_date": row["order_date"].strftime("%Y-%m-%d"),
+                    "daily_revenue": Decimal(str(row["daily_revenue"])),
+                    "avg_order_value": Decimal(str(row["avg_order_value"])),
+                    "avg_return_rate": Decimal(str(row["avg_return_rate"]))
+                }
+
+            table.put_item(Item=item)
+
+        except Exception as e:
+            logging.error(f"Failed to insert item for {row}: {e}")
 
 def read_csv(spark, path):
     logging.info(f"Reading CSV: {path}")
